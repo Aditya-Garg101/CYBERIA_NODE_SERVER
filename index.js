953117b2838a246bd5f1aad5bd39aced1a102ed1
Aditya-Garg101 <gargaditya880@gmail.com>
2024-11-18 23:50:16 +0530

    COmmits

--- index.js
+++ index.js
@@ -59,7 +59,8 @@ app.use(
     next();
   });
 
-app.get("/api/getkey",(req,res)=>res.status(200).json({key : process.env.RAZORPAY_API_KEY}));
+// app.get("/api/getkey",(req,res)=>res.status(200).json({key : process.env.RAZORPAY_API_KEY}));
+
 app.get("/",(req,res)=>{
     res.send("Helloi")
 })
