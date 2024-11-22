39aecd02b84c4ca64b97c141ce35af1a5e86bf2f
Aditya-Garg101 <gargaditya880@gmail.com>
2024-11-22 23:28:40 +0530

    comits by adi

--- controller/User.js
+++ controller/User.js
@@ -15,22 +15,6 @@ const admin = require("firebase-admin")
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
 
 
-
-
-
-
-// if(!admin.apps.length){
-//   admin.initializeApp({
-//     credential: admin.credential.cert({
-//       projectId: process.env.FIREBASE_PROJECT_ID,
-//       privateKey: process.env.FIREBASE_PRIVATE_KEY,
-//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//     }),
-//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   });
-// }
-
-
 if(!admin.apps.length){
   admin.initializeApp({
     credential:
